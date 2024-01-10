8c7fe180e04960746993283647f8e01a22328aa1
ihirad <53797949+ihirad@users.noreply.github.com>
2024-01-10 12:49:35 -0500

    edited databse

---
+++ quizz/prisma/migrations/20240110172555_updated_database/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_QuestionToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_QuestionToUser_AB_unique" ON "_QuestionToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_QuestionToUser_B_index" ON "_QuestionToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_QuestionToUser" ADD CONSTRAINT "_QuestionToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_QuestionToUser" ADD CONSTRAINT "_QuestionToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
