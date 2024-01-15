955b147cbb604d3c16e062da39a13f3080fc7290
belladoeswork <belladoeswork@gmail.com>
2024-01-16 04:21:27 +0800

    Hero fix

--- quizz/src/app/(pages)/game/classes/Player.jsx
+++ quizz/src/app/(pages)/game/classes/Player.jsx
@@ -1,27 +1,196 @@
-// import React from 'react';
+import { Sprite } from './Sprite.jsx';
+import collision from "../../../../helpers/utils.js";
+
+const gravity = 0.1;
+
+export class Player extends Sprite {
+  constructor({
+    position,
+    collisionBlocks,
+    platformCollisionBlocks,
+    imageSrc,
+    frameRate,
+    scale = 1,
+    context,
+    // animations,
+  }) {
+    super({ imageSrc, frameRate, scale });
+    this.position = position;
+    this.context = context;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.collisionBlocks = collisionBlocks;
+    this.platformCollisionBlocks = platformCollisionBlocks;
+    this.hitbox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      width: 10,
+      height: 10,
+    };
+    // this.animations = animations
+    // this.lastDirection = 'right'
+
+    // for (let key in this.animations) {
+    //   const image = new Image();
+    //   image.onload = () => {
+    //     this.animations[key].loaded = true;
+    //   };
+    //   image.src = this.animations[key].imageSrc;
+    //   this.animations[key].image = image;
+    // }
+  }
 
 
-// // player class
-// export class Player {
-//     constructor({position, context}) {
-//       this.position = position;
-//       this.context = context;
-//       this.velocity = {
-//         x: 0,
-//         y: 1,
-//       };
-//       this.height = 100;
-//     }
-//     draw() {
-//       context.fillStyle = "red";
-//       context.fillRect(this.position.x, this.position.y, 100, this.height);
-//     }
-//     update() {
-//       this.draw();
-//       this.position.x += this.velocity.x;
-//       this.position.y += this.velocity.y;
-//       if (this.position.y + this.height + this.velocity.y < canvas.height)
-//         this.velocity.y += gravity;
-//       else this.velocity.y = 0;
-//     }
-//   }
+  // switchSprite(key) { 
+  //   if (this.image === this.animations[key] || !this.loaded) return;
+  //   this.currentFrame = 0;
+  //   this.image = this.animations[key].image;
+  //   this.frameBuffer = this.animations[key].frameBuffer;
+  //   this.frameRate = this.animations[key].frameRate;
+
+  // }
+
+  
+  update() {
+    this.updateFrames();
+    this.updateHitBox();
+
+    this.draw();
+
+    // player
+    this.context.fillStyle = "rgba(0, 255, 0, 0.5)";
+    this.context.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+    // hitbox
+    this.context.fillStyle = "rgba(255, 0, 0, 0.5)";
+    this.context.fillRect(
+      this.hitbox.position.x,
+      this.hitbox.position.y,
+      this.hitbox.width,
+      this.hitbox.height
+    );
+
+
+    this.draw();
+    // this.hitbox.draw()
+    this.position.x += this.velocity.x;
+    // this.updateHitBox();
+    this.checkForHorizontalCollision();
+
+    this.position.y += this.velocity.y;
+    this.applyGravity();
+    // this.updateHitBox();
+    this.checkForVerticalCollision();
+
+  }
+
+  updateHitBox() {
+    this.hitbox = {
+      position: {
+        x: this.position.x + 15,
+        y: this.position.y + 13,
+      },
+      width: 35,
+      height: 54,
+    };
+
+  }
+
+
+  checkForHorizontalCollision() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const block = this.collisionBlocks[i];
+      if (
+        collision({
+          object1: this.hitbox,
+          object2: block,
+        })
+        
+      ) {
+
+        console.log('Collision detected!');
+        console.log('Player hitbox:', this.hitbox);
+        console.log('Block:', block);
+
+        if (this.velocity.x > 0) {
+          this.velocity.x = 0;
+
+          const offset =
+            this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+          this.position.x = block.position.x - offset - 0.01;
+          break;
+        }
+      
+        if (this.velocity.x < 0) {
+          this.velocity.x = 0;
+
+          const offset = this.hitbox.position.x - this.position.x;
+
+          this.position.x = block.position.x + block.width - offset + 0.01;
+          break;
+        }
+      }
+    }
+  }
+
+  applyGravity() {
+    this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
+// to rmv
+    // if (this.position.y + this.height > this.context.canvas.height) {
+    //   this.position.y = this.context.canvas.height - this.height;
+    //   this.velocity.y = 0;
+    // }
+  }
+
+  // check for player & collision intersection
+
+  checkForVerticalCollision() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const block = this.collisionBlocks[i];
+      if (
+        collision({
+          object1: this.hitbox,
+          object2: block,
+        })
+      ) {
+        console.log('Collision detected!');
+        console.log('Player hitbox:', this.hitbox);
+        console.log('Block:', block);
+        
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+
+
+          const offset =
+            this.hitbox.position.y - this.position.y + this.hitbox.height ;
+          
+          this.position.y = block.position.y - offset - 0.01;
+          // added? 
+          this.position.y -= 1; 
+          break;
+        }
+      
+      
+        else if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+
+          const offset = this.hitbox.position.y - this.position.y;
+
+          this.position.y = block.position.y + block.height - offset + 0.01;
+          break;
+        }
+      }
+    }
+  }
+
+}
--- quizz/src/app/(pages)/game/page.jsx
+++ quizz/src/app/(pages)/game/page.jsx
@@ -1,7 +1,10 @@
 "use client";
 
 import React, { useRef, useEffect } from "react";
-import { floorCollisions, platformCollisions } from "../../../components/data/Collisions.js";
+import {
+  floorCollisions,
+  platformCollisions,
+} from "../../../components/data/Collisions.js";
 import collision from "../../../helpers/utils.js";
 import { Sprite } from "./classes/Sprite.jsx";
 import { Player } from "./classes/Player.jsx";
@@ -22,30 +25,6 @@ export default function GameLevel1() {
       height: canvas.height / 4,
     };
 
-
-
-    class CollisionBlock {
-      constructor({ position }) {
-        this.position = position;
-
-        this.width = 16;
-        this.height = 16;
-      }
-
-      draw() {
-        context.fillStyle = "rgba(255, 0, 0, 0.5)";
-        context.fillRect(
-          this.position.x ,
-          this.position.y,
-          this.width,
-          this.height
-        );
-      }
-      update() {
-        this.draw();
-      }
-    }
-
     const floorCollisions2D = [];
     for (let i = 0; i < floorCollisions.length; i += 30) {
       floorCollisions2D.push(floorCollisions.slice(i, i + 30));
@@ -65,12 +44,14 @@ export default function GameLevel1() {
           symbol === 152
         ) {
           collisionBlocks.push(
-          new CollisionBlock({
-            position: {
-              x: x * 16,
-              y: y * 16,
-            },
-          }));
+            new CollisionBlock({
+              position: {
+                x: x * 16,
+                y: y * 16,
+              },
+              context: context,
+            })
+          );
         }
       });
     });
@@ -80,128 +61,52 @@ export default function GameLevel1() {
       platformCollisions2D.push(platformCollisions.slice(i, i + 30));
     }
 
-
     const platformCollisionBlocks = [];
     platformCollisions2D.forEach((row, y) => {
       row.forEach((symbol, x) => {
-        if (
-          symbol === 387 ||
-          symbol === 388 ||
-          symbol === 389
-        ) {
+        if (symbol === 387 || symbol === 388 || symbol === 389) {
           platformCollisionBlocks.push(
-          new CollisionBlock({
-            position: {
-              x: x * 16,
-              y: y * 16,
-            },
-          }));
+            new CollisionBlock({
+              position: {
+                x: x * 16,
+                y: y * 16,
+              },
+              context: context,
+              height: 4,
+            })
+          );
         }
       });
     });
 
-    // row 15 *16 = 240
-    // y 2 *16 = 32
-
-
-    const gravity = 0.1;
-
-    // sprite class
-    class Sprite {
-      constructor({ position, imageSrc }) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-        this.imageLoaded = false;
-        this.image.onload = () => {
-          this.imageLoaded = true;
-          this.scale = Math.max(
-            canvas.width / this.image.width,
-            canvas.height / this.image.height
-          );
-          this.position.y = canvas.height - this.image.height * this.scale;
-        };
-      }
-
-      draw() {
-        if (!this.image) return;
-        context.drawImage(
-          this.image,
-          this.position.x,
-          this.position.y,
-     
-        );
-      }
-      update() {
-        this.draw();
-      }
-    }
-
-    // player class
-    class Player {
-      constructor({position, collisionBlocks}) {
-        this.position = position;
-        this.velocity = {
-          x: 0,
-          y: 1,
-        };
-        this.height = 25;
-        this.width = 25;
-        this.collisionBlocks= collisionBlocks;
-      }
-      draw() {
-        context.fillStyle = "red";
-        context.fillRect(this.position.x*4, this.position.y*4, this.width *4, this.height*4);
-      }
-      update() {
-        this.draw();
-        this.checkForVerticalCollision();
-        this.position.x += this.velocity.x;
-        
-        this.applyGravity();
-        
-      }
-      applyGravity() {
-        this.position.y += this.velocity.y;
-        this.velocity.y += gravity;
-      }
-
-      // check for player & collision intersection
-
-      checkForVerticalCollision() { 
-        for (let i = 0; i < this.collisionBlocks.length; i++) {
-          const block = this.collisionBlocks[i];
-          if (
-            collision(
-              {
-                object1: this,
-                object2: block,
-              }
-            )
-          ) {
-            if (this.velocity.y > 0) {
-              this.velocity.y = 0;
-              this.position.y = block.position.y - this.height;
-            }
-          }
-            // console.log("collision");
-        }
-      }
-
-    }
+    // const gravity = 0.1;
 
     // draw new player
-
     const player = new Player({
       position: {
         x: 100,
-        y: 0,
+        y: 300,
       },
+      context: context,
       collisionBlocks,
+      platformCollisionBlocks,
+      imageSrc: "/assets/hero/Idle.png",
+      frameRate: 4,
+      // animations: {
+      //   Idle: {
+      //     imageSrc: "/assets/hero/Idle.png",
+      //     frameRate: 4,
+      //     frameBuffer: 3,
+      //   },
+      //   Run: {
+      //     imageSrc: "/assets/hero/Run.png",
+      //     frameRate: 8,
+      //     frameBuffer: 3,
+      //   }
+      // },
     });
 
     // player keys
-
     const keys = {
       ArrowRight: {
         pressed: false,
@@ -212,42 +117,64 @@ export default function GameLevel1() {
     };
 
     // getting the background image
-
     const background = new Sprite({
       position: {
         x: 0,
         y: 0,
       },
-      imageSrc: "/assets/level1.png",
+      imageSrc: "assets/level1.png",
+      context: context,
     });
 
     // animation loop
-
     function animate() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      window.requestAnimationFrame(animate);
       context.fillStyle = "white";
       context.fillRect(0, 0, canvas.width, canvas.height);
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      window.requestAnimationFrame(animate);
 
       // start transformations
       context.save();
       context.scale(4, 4);
       context.translate(0, -background.image.height + scaledCanvas.height);
-      
+
       // draw bg
       background.update();
-        // collision blocks
+      // collision blocks
       collisionBlocks.forEach((CollisionBlock) => CollisionBlock.update());
-      
+
       //platform collision blocks
       platformCollisionBlocks.forEach((block) => block.update());
 
-      // end transformations
-      context.restore();
+     
+      // player.velocity.x = 0;
+      // if (keys.ArrowRight.pressed) {
+      //   // player.switchSprite("Run");
+      //   player.velocity.x = 2;
+      //   player.lastDirection = "right";
+      // } else if (keys.ArrowLeft.pressed) {
+      //   // player.switchSprite("Run");
+      //   player.velocity.x = -2;
+      //   player.lastDirection = "left";
+      // } else if (player.velocity.y === 0) {
+      //   player.velocity.x = 0;
+      //   if (player.lastDirection === "right") player.switchSprite("Idle");
+      //   // else player.switchSprite("Idle");
+      // }
 
-    
+
+
+      
+            // end transformations
+      context.restore();
+      
+
+       // // draw player
+       player.update();
+
+
       // draw player
-      player.update();
+      // player.update();
     }
 
     animate();
@@ -256,18 +183,18 @@ export default function GameLevel1() {
     window.addEventListener("keydown", (e) => {
       switch (e.key) {
         case "ArrowUp":
-          player.velocity.y = -20;
+          player.velocity.y = -4;
           break;
         case "ArrowDown":
           player.velocity.y = 1;
           break;
         case "ArrowLeft":
           keys.ArrowLeft.pressed = true;
-          player.velocity.x = -5;
+          player.velocity.x = -2;
           break;
         case "ArrowRight":
           keys.ArrowRight.pressed = true;
-          player.velocity.x = 5;
+          player.velocity.x = 2;
           break;
       }
     });
