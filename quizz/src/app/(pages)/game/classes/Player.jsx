da09523912fecfc7efab39b36041e142a5733124
belladoeswork <belladoeswork@gmail.com>
2024-01-17 18:27:07 +0800

    other fixes with char

--- quizz/src/app/(pages)/game/classes/Player.jsx
+++ quizz/src/app/(pages)/game/classes/Player.jsx
@@ -32,40 +32,31 @@ export class Player extends Sprite {
       height: 10,
     };
 
-    // Find the highest collision block
-    // let highestCollisionBlock = this.collisionBlocks.reduce((highest, block) => {
-    //   return (highest && highest.position.y < block.position.y) ? highest : block;
-    // }, null);
-
-    // // Set the player's y position to be just above the highest collision block
-    // this.position.y = highestCollisionBlock.position.y - this.hitbox.height;
     // this.animations = animations
     // this.lastDirection = 'right'
 
     // for (let key in this.animations) {
-    //   const image = new Image();
-    //   image.onload = () => {
-    //     this.animations[key].loaded = true;
-    //   };
-    //   image.src = this.animations[key].imageSrc;
-    //   this.animations[key].image = image;
+    //   const image = new Image()
+    //   image.src = this.animations[key].imageSrc
+
+    //   this.animations[key].image = image
     // }
   }
 
   // switchSprite(key) {
-  //   if (this.image === this.animations[key] || !this.loaded) return;
-  //   this.currentFrame = 0;
-  //   this.image = this.animations[key].image;
-  //   this.frameBuffer = this.animations[key].frameBuffer;
-  //   this.frameRate = this.animations[key].frameRate;
+  //   if (this.image === this.animations[key].image || !this.loaded) return
 
+  //   this.currentFrame = 0
+  //   this.image = this.animations[key].image
+  //   this.frameBuffer = this.animations[key].frameBuffer
+  //   this.frameRate = this.animations[key].frameRate
   // }
 
   update() {
     this.updateFrames();
     this.updateHitBox();
 
-    this.draw();
+    // this.draw();
 
     // player
     this.context.fillStyle = "rgba(0, 255, 0, 0.5)";
@@ -85,15 +76,19 @@ export class Player extends Sprite {
     );
 
     this.draw();
-    this.checkForHorizontalCollision();
 
     this.position.x += this.velocity.x;
+    // this.applyGravity();
     this.updateHitBox();
+    this.checkForHorizontalCollisions();
+
+    this.position.y += this.velocity.y;
+    // this.applyGravity();
+    this.updateHitBox();
+    this.checkForVerticalCollisions();
 
     this.applyGravity();
     this.updateHitBox();
-    this.checkForVerticalCollision();
-    this.position.y += this.velocity.y;
   }
 
   updateHitBox() {
@@ -107,163 +102,79 @@ export class Player extends Sprite {
     };
   }
 
-  checkForHorizontalCollision() {
+  checkForHorizontalCollisions() {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
-      const block = this.collisionBlocks[i];
+      const collisionBlock = this.collisionBlocks[i];
+
       if (
         collision({
           object1: this.hitbox,
-          object2: block,
+          object2: collisionBlock,
         })
       ) {
-        console.log("Collision detected!");
-        console.log("Player hitbox:", this.hitbox);
-        console.log("Block:", block);
-
         if (this.velocity.x > 0) {
           this.velocity.x = 0;
 
           const offset =
             this.hitbox.position.x - this.position.x + this.hitbox.width;
 
-          this.position.x = block.position.x - offset - 0.01;
+          this.position.x = collisionBlock.position.x - offset - 0.01;
           break;
-        } else if (this.velocity.x < 0) {
+        }
+
+        if (this.velocity.x < 0) {
           this.velocity.x = 0;
 
           const offset = this.hitbox.position.x - this.position.x;
 
-          this.position.x = block.position.x + block.width - offset + 0.01;
+          this.position.x =
+            collisionBlock.position.x + collisionBlock.width - offset + 0.01;
           break;
         }
       }
     }
   }
 
-    applyGravity() {
-      // this.position.y += this.velocity.y;
-      this.velocity.y += gravity;
-  // to rmv
-      if (this.position.y + this.height > this.context.canvas.height) {
-        this.position.y = this.context.canvas.height - this.height;
-        this.velocity.y = 0;
-      }
-      }
+  applyGravity() {
+    // this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
+    // to rmv
+    if (this.position.y + this.height > this.context.canvas.height) {
+      this.position.y = this.context.canvas.height - this.height;
+      this.velocity.y = 0;
+    }
+  }
 
-  // applyGravity() {
-  //   this.velocity.y += gravity;
-  //   // Predict the next position
-  //   let nextY = this.position.y + this.velocity.y;
-  //   // Check for collision with each collision block
-  //   for (let block of this.collisionBlocks) {
-  //     if (
-  //       collision({
-  //         object1: this.hitbox,
-  //         object2: block,
-  //       })
-  //     ) {
-  //       nextY = block.position.y - this.hitbox.height;
-  //       this.velocity.y = 0;
-  //       break;
-  //     }
-  //   }
-  
-  //   if (nextY + this.height > this.context.canvas.height) {
-  //     nextY = this.context.canvas.height - this.height;
-  //     this.velocity.y = 0;
-  //   }
-  
-  //   // Only update the player's y position if a collision is not detected
-  //   if (!collision({
-  //     object1: this.hitbox,
-  //     object2: block,
-  //   })) {
-  //     this.position.y = nextY;
-  //   }
-  // }
-
-  // Check if the next position is beyond the canvas
-  // if (nextY + this.height > this.context.canvas.height) {
-  //   nextY = this.context.canvas.height - this.height;
-  //   this.velocity.y = 0;
-  // }
-
-  // // Update the position
-  // this.position.y = nextY;
-  
-
-  //   // Update the position
-  //   this.position.y = nextY;
-  // }
-
-  // check for player & collision intersection
-
-  checkForVerticalCollision() {
+  checkForVerticalCollisions() {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
-      const block = this.collisionBlocks[i];
+      const collisionBlock = this.collisionBlocks[i];
+
       if (
         collision({
           object1: this.hitbox,
-          object2: block,
+          object2: collisionBlock,
         })
       ) {
-        console.log("Collision detected!");
-        console.log("Player hitbox:", this.hitbox);
-        console.log("Block:", block);
-
         if (this.velocity.y > 0) {
           this.velocity.y = 0;
 
           const offset =
             this.hitbox.position.y - this.position.y + this.hitbox.height;
 
-          this.position.y = block.position.y - offset - 0.01;
-          // added?
-          console.log("Player position after collision:", this.position);
+          this.position.y = collisionBlock.position.y - offset - 0.01;
           break;
-        } else if (this.velocity.y < 0) {
+        }
+
+        if (this.velocity.y < 0) {
           this.velocity.y = 0;
 
           const offset = this.hitbox.position.y - this.position.y;
 
-          this.position.y = block.position.y + block.height - offset + 0.01;
-          console.log("Player position after collision:", this.position);
+          this.position.y =
+            collisionBlock.position.y + collisionBlock.height - offset + 0.01;
           break;
         }
       }
     }
   }
 }
-//   checkForVerticalCollision() {
-//     for (let i = 0; i < this.collisionBlocks.length; i++) {
-//       const block = this.collisionBlocks[i];
-//       if (
-//         collision({
-//           object1: this.hitbox,
-//           object2: block,
-//         })
-//       ) {
-//         console.log("Collision detected!");
-//         console.log("Player hitbox:", this.hitbox);
-//         console.log("Block:", block);
-//         if (this.velocity.y > 0) {
-//           this.velocity.y = 0;
-//           const offset =
-//             this.hitbox.position.y - this.position.y + this.hitbox.height;
-//           this.position.y = block.position.y - offset - 0.01;
-//           console.log("Player position after collision:", this.position);
-//           break;
-//         } else if (this.velocity.y < 0) {
-//           this.velocity.y = 0;
-//           const offset = this.hitbox.position.y - this.position.y;
-//           this.position.y = block.position.y + block.height - offset + 0.01;
-//           console.log("Player position after collision:", this.position);
-//           break;
-//         }
-//       } else {
-//         // Only update the player's y position if a collision is not detected
-//         this.position.y += this.velocity.y;
-//       }
-//     }
-//   }
-// }
--- quizz/src/app/app.js
+++ quizz/src/app/app.js
@@ -1,34 +1,34 @@
-"use client"
+// "use client"
 
 
-// import Sprite from "../components/object-graphics/Sprite.js";
-import { useEffect, useState } from "react";
-import { SPRITE_SHEET_SRC } from "../helpers/consts.js";
-import RenderLevel from "../components/data/RenderLevel.js";
+// // import Sprite from "../components/object-graphics/Sprite.js";
+// import { useEffect, useState } from "react";
+// import { SPRITE_SHEET_SRC } from "../helpers/consts.js";
+// import RenderLevel from "../components/data/RenderLevel.js";
 
 
 
-export default function App() {
+// export default function App() {
 
-    const [spriteSheetImage, setSpriteSheetImage] = useState(null);
+//     const [spriteSheetImage, setSpriteSheetImage] = useState(null);
  
-   useEffect(() => {
-     const image = new Image();
-     image.src = SPRITE_SHEET_SRC;
-     image.onload = () => {
-       setSpriteSheetImage(image);
-     };
-   }, []);
+//    useEffect(() => {
+//      const image = new Image();
+//      image.src = SPRITE_SHEET_SRC;
+//      image.onload = () => {
+//        setSpriteSheetImage(image);
+//      };
+//    }, []);
  
-   if (!spriteSheetImage) {
-     return null;
-   }
+//    if (!spriteSheetImage) {
+//      return null;
+//    }
 
 
-    return (
-        <div>
-            <p>The root of the game</p>
-            return <RenderLevel spriteSheetImage={spriteSheetImage} />;
-            </div>
-    )
-}
+//     return (
+//         <div>
+//             <p>The root of the game</p>
+//             return <RenderLevel spriteSheetImage={spriteSheetImage} />;
+//             </div>
+//     )
+// }
